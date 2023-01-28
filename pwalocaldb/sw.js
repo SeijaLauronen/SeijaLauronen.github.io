8eac6ee37c783cf300642196cb3f9e7588f1b8ee
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-01-28 10:27:38 +0200

    cahe ja update

--- pwalocaldb/sw.js
+++ pwalocaldb/sw.js
@@ -3,10 +3,12 @@ var filesToCache = [
   '/',
   '/index.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+  'https://unpkg.com/localbase/dist/localbase.min.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
+// kts https://www.youtube.com/watch?v=kT3qSf7jG5c&list=PL4cUxeGkcC9gTxqJBcDmoi5Q2pzDusSL7&index=15
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -16,6 +18,8 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
+// kts https://www.youtube.com/watch?v=0mAw9Na6hyM&list=PL4cUxeGkcC9gTxqJBcDmoi5Q2pzDusSL7&index=16
+// hakee ensin cahesta ja jos ei löydy, niin sitten serveriltä
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
