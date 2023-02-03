12c66b23501b50bbfbbe57600224fbfca49e889e
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-02-03 20:56:24 +0200

    Vaihdettu sidemenun ja sideformin avautumissuuntaa

--- pwaUI/js/ui.js
+++ pwaUI/js/ui.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     // nav menu
     const menus = document.querySelectorAll('.side-menu');
-    M.Sidenav.init(menus, {edge: 'right'});
+    M.Sidenav.init(menus, {edge: 'left'});
+    //alert("left");
     // add recipe form
     const forms = document.querySelectorAll('.side-form');
-    M.Sidenav.init(forms, {edge: 'left'});
+    M.Sidenav.init(forms, {edge: 'right'});
+    //alert("right");
   });
