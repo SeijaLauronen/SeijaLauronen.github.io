c62a33e2dc40bdd69a114d30abd38c5e4dbaf126
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-02-04 17:22:26 +0200

    preCache (15)

--- pwaUI/sw.js
+++ pwaUI/sw.js
@@ -1,14 +1,38 @@
+const staticCacheName = 'pwaui-static-3';
+// nämä on kutsuja, siksi tuo / on se yksi kutsu... ei siis taida viitata hakemsitoon?!
+const assets = [
+  '/',
+  'index.html',
+  'js/main.js',
+  'js/ui.js',
+  'js/materialize.min.js',
+  'css/styles.css',
+  'css/materialize.min.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
+
+
+
 // install event
 self.addEventListener('install', evt => {
-  console.log('service worker installed');
+  //console.log('service worker installed');
+  // install voi kestää liian vähän aikaa, että chaetus kerkeäisi tapahtua, siksi odotetaan cachetus
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker activated');
+  //console.log('service worker activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
 });
