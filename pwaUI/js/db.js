3a966613d683fda2cfbeefead181213b573b03f0
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-02-19 10:28:50 +0200

    IndexDB yhteys toimii

---
+++ pwaUI/js/db.js
@@ -0,0 +1,19 @@
+
+function listaa(){
+    //alert("listaan");
+    db.collection('users').get().then(users => {
+                  //resultset.value =users[0].name
+
+                  let listOfNames ="";
+                  users.forEach(element => {
+                    listOfNames+=element.id + ":"
+                    listOfNames+=element.name + ", "
+                    //addItemToTable(element.id,element.name);
+                  });
+
+                  //resultset.value =listOfNames
+                  console.log(listOfNames);
+                  
+                })
+
+  }
