8bcad6f041756b6d2b0fb95d3d21a1e082f0987c
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-03-11 17:48:15 +0200

    Alaosan navigointi alustavasti

--- pwaUI/sw.js
+++ pwaUI/sw.js
@@ -1,6 +1,6 @@
-const programVersion = '261';
-const staticCacheName = 'pwaui-static-261';
-const dynamicCacheName = 'pwaui-dynamic-261'; // tämäkin joutaisi pois, mutta jätetään toistaiseksi..
+const programVersion = '277';
+const staticCacheName = 'pwaui-static-277';
+const dynamicCacheName = 'pwaui-dynamic-277'; // tämäkin joutaisi pois, mutta jätetään toistaiseksi..
 // nämä on kutsuja, siksi tuo / on se yksi kutsu... ei siis taida viitata hakemistoon?!
 // Add napista tuli offline tilassa page not found, siinä urlissa oli perässä kyssäri, niin laitoin myös sen tähän.
 // myös kun laittoi dynaamisen cahen, niin jos oli käynyt painamassa online tilassa Addnappi, niin se toimi
