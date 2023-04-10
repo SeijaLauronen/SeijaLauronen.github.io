89d5265d638b19ac5344353ba9c99e57088a1803
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-04-10 10:40:06 +0300

    Pitkä tuotenimi tulee ok

--- pwaUI/sw.js
+++ pwaUI/sw.js
@@ -1,6 +1,6 @@
-const programVersion = '2023-04-09: 808';
-const staticCacheName = 'pwaui-static-808'; 
-const dynamicCacheName = 'pwaui-dynamic-808'; // tämäkin joutaisi pois, mutta jätetään toistaiseksi..
+const programVersion = '2023-04-09: 829';
+const staticCacheName = 'pwaui-static-829'; 
+const dynamicCacheName = 'pwaui-dynamic-829'; // tämäkin joutaisi pois, mutta jätetään toistaiseksi..
 // nämä on kutsuja, siksi tuo / on se yksi kutsu... ei siis taida viitata hakemistoon?!
 // Add napista tuli offline tilassa page not found, siinä urlissa oli perässä kyssäri, niin laitoin myös sen tähän.
 // myös kun laittoi dynaamisen cahen, niin jos oli käynyt painamassa online tilassa Addnappi, niin se toimi
