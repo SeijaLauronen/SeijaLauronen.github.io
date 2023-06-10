2afc9945f92752a8f18be8794f7cbf37813f890f
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2023-06-10 21:30:01 +0300

    DB sisältö DATA välilehdelle

--- pwaUI/sw.js
+++ pwaUI/sw.js
@@ -1,6 +1,6 @@
-const programVersion = '2023-04-29: 954';
-const staticCacheName = 'pwaui-static-954'; 
-const dynamicCacheName = 'pwaui-dynamic-954'; // tämäkin joutaisi pois, mutta jätetään toistaiseksi..
+const programVersion = '2023-06-10: 957';
+const staticCacheName = 'pwaui-static-957'; 
+const dynamicCacheName = 'pwaui-dynamic-957'; // tämäkin joutaisi pois, mutta jätetään toistaiseksi..
 // nämä on kutsuja, siksi tuo / on se yksi kutsu... ei siis taida viitata hakemistoon?!
 // Add napista tuli offline tilassa page not found, siinä urlissa oli perässä kyssäri, niin laitoin myös sen tähän.
 // myös kun laittoi dynaamisen cahen, niin jos oli käynyt painamassa online tilassa Addnappi, niin se toimi
